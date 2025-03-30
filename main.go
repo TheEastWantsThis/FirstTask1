23a35732a58c9951765d1d2f4a7939e1e1153d3e
TheEastWantsThis <reworldtoday@gmail.com>
2025-03-30 17:34:45 +0300

    "С учетом третьего задания"

--- main.go
+++ main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/gorilla/mux"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func TaskHandler(w http.ResponseWriter, r *http.Request) {
@@ -13,6 +14,31 @@ func TaskHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(req)
 
+}
+
+func PatchHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var updates map[string]interface{}
+	json.NewDecoder(r.Body).Decode(&updates)
+
+	DB.Model(&Task{}).Where("id = ?", id).Updates(updates)
+
+	var task Task
+	DB.First(&task, id)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(task)
+}
+
+func DeleteHandler(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	DB.Delete(&Task{}, id)
+
+	w.WriteHeader(http.StatusNoContent)
 }
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
@@ -27,7 +53,9 @@ func main() {
 	DB.AutoMigrate(&Task{})
 
 	router := mux.NewRouter()
+	router.HandleFunc("/api/task/{id}", PatchHandler).Methods("PATCH")
 	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
 	router.HandleFunc("/api/task", TaskHandler).Methods("POST")
+	router.HandleFunc("/api/task/delete/{id}", DeleteHandler).Methods("DELETE")
 	http.ListenAndServe(":8000", router)
 }
